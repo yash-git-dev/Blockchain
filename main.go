7f749ab48386b6ec208b3a95f547277e3b984ece
yash <yashgudhka98@gmail.com>
2024-06-13 21:19:17 +0530

    pow

--- main.go
+++ main.go
@@ -1,60 +1,24 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+
+	blockchain "github.com/yash-git-dev/go-blockchain/Blockchain"
 )
 
-type Block struct {
-	PrevHash []byte
-	Data     []byte
-	Hash     []byte
-}
-
-type Blockchain struct {
-	blocks []*Block
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	var b = &Block{
-		Data:     []byte(data),
-		PrevHash: prevHash,
-	}
-
-	b.DeriveHash()
-
-	return b
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func (chain *Blockchain) AddBlock(data string) {
-	lastBlock := chain.blocks[len(chain.blocks)-1]
-	b := CreateBlock(data, lastBlock.Hash)
-	chain.blocks = append(chain.blocks, b)
-}
-
-func InitBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{Genesis()}}
-}
-
 func main() {
-	chain := InitBlockchain()
+	chain := blockchain.InitBlockchain()
 
 	chain.AddBlock("abc")
 	chain.AddBlock("pqs")
 	chain.AddBlock("xyz")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
+
 		fmt.Printf("data in block:%x\n", block.Hash)
+		fmt.Printf("previous data in block:%x\n", block.PrevHash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Println(pow.Validate())
 	}
 }
